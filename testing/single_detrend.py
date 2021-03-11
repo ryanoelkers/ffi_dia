533d3276e6c3c11957cbe288435a1cca9c709357
Ryan J. Oelkers <ryanjamesoelkers@gmail.com>
2021-03-10 19:43:56 -0600

    updated single_detrend.py to run on raw lightcurves

--- testing/single_detrend.py
+++ testing/single_detrend.py
@@ -7,8 +7,9 @@ import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('TkAgg')
 
-# set this flag to 1 if you want to experiment with the new SPEARMAN RANK method
-EXPERIMENT = 0
+# set this flag to be SPEARMAN or ZEROPOINT depending on how you want to detrend
+METHOD = 'SPEARMAN'
+SPEARMAN_CUT = 0.85
 
 # change this for the sector camera ccd you are looking for
 SECTOR = 'sector01'
@@ -20,14 +21,19 @@ TREND_STARS = 500
 MG_CLIP = 13
 
 # change these for the specific directories
+# SECTOR_DIRECTORY = '/media/oelkerrj/Yavin/TESS/data/'
 SECTOR_DIRECTORY = '/net/jovan/export/jovan/oelkerrj/FFI/'
+# MASTER_DIRECTORY = SECTOR_DIRECTORY + SECTOR + '/master/'
 MASTER_DIRECTORY = SECTOR_DIRECTORY + SECTOR + '/master/star_list/'
 RAW_DIRECTORY = SECTOR_DIRECTORY + SECTOR + '/lc/raw/'
+DETREND_DIRECTORY = SECTOR_DIRECTORY + SECTOR + '/lc/detrend_test/'
 OUTPUT_DIRECTORY = SECTOR_DIRECTORY + SECTOR + '/output/'  # UPDATE THIS DIRECTORY TO BE WHERE YOU WANT TO WRITE TO
 
 # read in the master magnitude file from the master frame directory
 master = pd.read_csv(MASTER_DIRECTORY + SECTOR + '_' + CAMERA + '_' + CCD + '_master.ap',
                      delimiter=' ', names=['TICID', 'x', 'y', 'tessmag', 'mag', 'er', 'flux', 'flux_er'])
+# master = pd.read_csv(MASTER_DIRECTORY + SECTOR + '_' + CAMERA + '_' + CCD + '_master.data',
+#                     delimiter=',', index_col=0)
 
 # push TICIDs to a list
 ticid = master['TICID'].to_list()
@@ -35,66 +41,100 @@ master_mags = master['mag'].to_numpy()
 zpt_ticid = master[master['tessmag'] < MG_CLIP].TICID.to_list()
 zpt_mags = master[master['tessmag'] < MG_CLIP]['mag'].to_numpy()
 
-filenames = [RAW_DIRECTORY + str(x) + "_" + SECTOR + "_" + CAMERA + "_" + CCD + ".lc" for x in zpt_ticid]
+# set up the light curve vectors based on the method you are using
+if METHOD == 'SPEARMAN':
+    # read in the necessary light curves
+    filenames = [RAW_DIRECTORY + str(x) + "_" + SECTOR + "_" + CAMERA + "_" + CCD + ".lc" for x in zpt_ticid]
 
-# read in the raw lightcurves to find the zeropoint
-lc_data = np.array([np.genfromtxt(f, delimiter=' ')[:, 1] for f in filenames])
+    # read in the raw lightcurves to find the zeropoint
+    # lc_data = np.array([np.genfromtxt(f, delimiter=' ')[:, 2] for f in filenames[0:TREND_STARS]])
+    lc_data = np.array([np.genfromtxt(f, delimiter=' ')[:, 1] for f in filenames[0:TREND_STARS]])
+    # subtract the master frame magnitude
+    median_df = pd.DataFrame(lc_data).replace('*********', 99.999999).apply(lambda x: x-x.median(), axis=1)
 
-# subtract the master frame magnitude, and then median combine to determine the zeropoint trend
-median_df = pd.DataFrame(lc_data).replace('*********', 99.999999) - zpt_mags.reshape(-1, 1)
-zpt_offset = median_df.median(axis=0)
 
-if EXPERIMENT == 1:
-    # make the experimental dataframe
-    exp_array = median_df[0:TREND_STARS]
-    exp_df = pd.DataFrame(exp_array)
+if METHOD == 'ZEROPOINT':
+    filenames = [DETREND_DIRECTORY + str(x) + "_" + SECTOR + "_" + CAMERA + "_" + CCD + ".lc" for x in zpt_ticid]
+
+    # read in the raw lightcurves to find the zeropoint
+    lc_data = np.array([np.genfromtxt(f, delimiter=' ')[:, 2] for f in filenames])
+
+    # subtract the master frame magnitude
+    median_df = pd.DataFrame(lc_data).replace('*********', 99.999999) - zpt_mags.reshape(-1, 1)
+
+    # calculat the zeropoint offset
+    zpt_offset = median_df.median(axis=0)
 
 iter_num = 0
 for idx, tic in enumerate(ticid):
-    # read in the raw light curve
-    lc = pd.read_csv(RAW_DIRECTORY + str(tic) + "_" + SECTOR + "_" + CAMERA + "_" + CCD + ".lc",
-                     names=['JD', 'mag', 'err'], sep=" ", na_values='*********')
 
-    # this is the current version of 'clean' in the pipeline
-    lc['clean'] = lc['mag'] - zpt_offset
+    if METHOD == 'ZEROPOINT':
+        # read in the raw light curve
+        lc = pd.read_csv(RAW_DIRECTORY + str(tic) + "_" + SECTOR + "_" + CAMERA + "_" + CCD + ".lc",
+                         names=['JD', 'detrend', 'raw', 'err'], sep=" ", na_values='*********')
 
-    if (idx % 100 == 0) & (idx > 0):
-        print("100 stars have been zeropoint corrected in sector: " + SECTOR + " camera: " +
-              CAMERA + " ccd: " + CCD + ". Now working on the next 100. " +
-              str(len(ticid) - idx - 1) + " stars remain to be zeropoint corrected.")
+        # this is the current version of 'clean' in the pipeline
+        lc['clean'] = lc['detrend'] - zpt_offset
 
-    # if you'd like to experiment with the SPEARMAN RANK gradient method it will be done here
-    if EXPERIMENT == 0:
         # write the cleaned light curve
-        lc[['JD', 'clean', 'mag', 'err']].to_csv(OUTPUT_DIRECTORY +
-                                                 str(tic) + "_" + SECTOR + "_" +
-                                                 CAMERA + "_" + CCD + ".lc", sep=" ",
-                                                 float_format='%.6f', header=False, index=False)
-    else:
+        lc[['JD', 'clean', 'detrend', 'raw', 'err']].to_csv(OUTPUT_DIRECTORY +
+                                                            str(tic) + "_" + SECTOR + "_" +
+                                                            CAMERA + "_" + CCD + ".lc", sep=" ",
+                                                            float_format='%.6f', header=False, index=False)
+
+        if (idx % 100 == 0) & (idx > 0):
+            print("100 stars have been zeropoint corrected in sector: " + SECTOR + " camera: " +
+                  CAMERA + " ccd: " + CCD + ". Now working on the next 100. " +
+                  str(len(ticid) - idx - 1) + " stars remain to be zeropoint corrected.")
+
+    if METHOD == 'SPEARMAN':
+        # read in the raw light curve
+        lc = pd.read_csv(RAW_DIRECTORY + str(tic) + "_" + SECTOR + "_" + CAMERA + "_" + CCD + ".lc",
+                         names=['JD', 'raw', 'err'], sep=" ", na_values='*********', usecols=[0, 2, 3])
 
         # get the light curve offset from median
-        exp_lc = lc['clean'] - lc['clean'].median()
+        spear_lc = lc['raw'] - lc['raw'].median()
 
         # get the spearman rank relationship for each light curve
-        corr_lc = exp_df.apply(lambda x: x.corr(pd.Series(exp_lc), 'spearman'), axis=1)
+        corr_lc = median_df.apply(lambda x: x.corr(pd.Series(spear_lc), 'spearman'), axis=1)
 
-        # check for like stars
-        if len(corr_lc[(corr_lc > 0.85) & (corr_lc < .99999)]) > 1:
+        # check for like stars based on the spearman rank
+        if len(corr_lc[(corr_lc > SPEARMAN_CUT) & (corr_lc < .99999)]) >= 1:
+            # be sure to remove any likely variables based on the standard deviation
+            std_df = median_df[(corr_lc > SPEARMAN_CUT) & (corr_lc < .99999)].std(axis=1)
+
             # pull out stars with high spearman ranks correlation
-            trend = np.average(exp_df[(corr_lc > 0.85) & (corr_lc < .99999)],
-                               weights=corr_lc[(corr_lc > 0.85) & (corr_lc < .99999)], axis=0)
-            lc['detrend'] = lc['clean'] - trend
+            trend = np.average(median_df[(corr_lc > SPEARMAN_CUT) &
+                                         (corr_lc < .99999) &
+                                         (std_df < std_df.min() * 3)],
+                               weights=corr_lc[(corr_lc > SPEARMAN_CUT) &
+                                               (corr_lc < .99999) &
+                                               (std_df < std_df.min() * 3)], axis=0)
+            lc['detrend'] = lc['raw'] - trend
         else:
-            lc['detrend'] = -99.999999
+            if len(corr_lc[(corr_lc > SPEARMAN_CUT - 0.05) & (corr_lc < .99999)]) >= 1:
+                # be sure to remove any likely variables based on the standard deviation
+                std_df = median_df[(corr_lc > SPEARMAN_CUT - 0.05) & (corr_lc < .99999)].std(axis=1)
 
-            # remove NaN values
-            lc = lc.fillna(-9.999999)
+                # pull out stars with high spearman ranks correlation
+                trend = np.average(median_df[(corr_lc > SPEARMAN_CUT - 0.05) &
+                                             (corr_lc < .99999) &
+                                             (std_df < std_df.min() * 3)],
+                                   weights=corr_lc[(corr_lc > SPEARMAN_CUT - 0.05) &
+                                                   (corr_lc < .99999) &
+                                                   (std_df < std_df.min() * 3)], axis=0)
+                lc['detrend'] = lc['raw'] - trend
+            else:
+                lc['detrend'] = lc['raw']
 
-            # write the detrended light curve
-            lc[['JD', 'detrend', 'clean', 'mag', 'err']].to_csv(OUTPUT_DIRECTORY +
-                                                                str(tic) + "_" + SECTOR + "_" +
-                                                                CAMERA + "_" + CCD + ".lc", sep=" ",
-                                                                float_format='%.6f', header=False, index=False)
+        # remove NaN values
+        lc = lc.fillna(-9.999999)
+
+        # write the detrended light curve
+        lc[['JD', 'detrend', 'raw', 'err']].to_csv(DETREND_DIRECTORY +
+                                                   str(tic) + "_" + SECTOR + "_" +
+                                                   CAMERA + "_" + CCD + ".lc", sep=" ",
+                                                   float_format='%.6f', header=False, index=False)
 
         # we are looking for stars within +/- 200 nearest magnitudes
         if (idx % 100 == 0) & (idx > 300):
@@ -107,13 +147,14 @@ for idx, tic in enumerate(ticid):
             # get the light curves for the initial number of light curves
             fname_hold = [RAW_DIRECTORY + str(x) + "_" + SECTOR + "_" + CAMERA + "_" + CCD + ".lc"
                           for x in ticid[lw_bnd:up_bnd]]
-            raw_hold = np.array([np.genfromtxt(f, delimiter=' ')[:, 1] for f in filenames])
+            # raw_hold = np.array([np.genfromtxt(f, delimiter=' ')[:, 2] for f in fname_hold])
+            raw_hold = np.array([np.genfromtxt(f, delimiter=' ')[:, 1] for f in fname_hold])
             median_hold = pd.DataFrame(raw_hold).replace('*********', 99.999999) - \
                           np.median(raw_hold, axis=1).reshape(-1, 1)
 
             # add the next 100 stars, clip the top 100 and reorganize the data frame
-            exp_df = exp_df.append(median_hold)
-            exp_df = exp_df.drop(np.arange(100)).reset_index(drop=True)
+            median_df = median_df.append(median_hold)
+            median_df = median_df.drop(np.arange(100)).reset_index(drop=True)
 
             iter_num += 1
 
